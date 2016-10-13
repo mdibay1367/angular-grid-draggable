fe4a2d1cf0aef7195c5d86be122d544cc98e9246
mdibay1367 <mohammad.dibay@gmail.com>
2016-10-12 19:18:06 -0700

    GridItem Directive is working.

--- app/grid/grid.directive.js
+++ app/grid/grid.directive.js
@@ -8,13 +8,14 @@
   function tmuiGrid($window) {
       var directive = {
         restrict: 'E',
-        transclude: false,
+        transclude: true,
+        replace: true,
         templateUrl: 'app/grid/grid.html',
         controllerAs: 'vm',
         scope: {
-          items: "=",
-          direction: "=",
-          directionLimit: "="
+          items: "=?",
+          direction: "=?",
+          directionLimit: "=?"
         },
         controller: Controller,
         bindToController: true,
@@ -25,37 +26,27 @@
       Controller.$inject = ['$scope']
       function Controller($scope, $element, $window) {
         var vm = this;
-        vm.grid = null;
-        vm.moveToPosition = moveToPosition;
-        vm.resizeGrid = resizeGrid;
-
-        vm.item3 = {x: 3, y: 15, w: 3, h: 4};
-
-        function moveToPosition() {
-        }
-
-        function resizeGrid() {
-        }
       }
-      function link(scope, el, attrs, ctrl) {
-        scope.vm.items.forEach(item =>
-          el.find('ul').append(
-            angular.element(
-              "<li class='ui-draggable'> <div class='inner'>"+
-              '<div class="controls">' +
-              '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
-              '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
-              '</div>' +
-              item.type +
-              '</div> </li>')
-              .attr({
-                'data-w': item.w,
-                'data-h': item.h,
-                'data-x': item.x,
-                'data-y': item.y
-              }))
-        );
 
+      function link(scope, el, attrs, ctrl) {
+        // scope.vm.items.forEach(item =>
+        //   el.find('ul').append(
+        //     angular.element(
+        //       "<li class='ui-draggable'> <div class='inner'>"+
+        //       '<div class="controls">' +
+        //       '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
+        //       '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
+        //       '</div>' +
+        //       item.type +
+        //       '</div> </li>')
+        //       .attr({
+        //         'data-w': item.w,
+        //         'data-h': item.h,
+        //         'data-x': item.x,
+        //         'data-y': item.y
+        //       }))
+        // );
+        // console.log(2);
         el.find('ul').gridList({
           lanes: 9,
           widthHeightRatio: 264 / 294,
--- app/grid/gridItem.directive.js
+++ app/grid/gridItem.directive.js
@@ -13,29 +13,8 @@
         scope: {
           item: "="
         },
-        template: '<li class="ui-draggable"> <div class="inner">'+
-        '<div class="controls">' +
-        '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
-        '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
-        '<a href="#zoom3" class="resize" data-w="3" data-h="1">3x1</a>' +
-        '<a href="#zoom1" class="resize" data-w="1" data-h="2">1x2</a>' +
-        '<a href="#zoom2" class="resize" data-w="2" data-h="2">2x2</a>' +
-        '</div>' +
-        '<ng-transclude></ng-transclude>' +
-        '</div> </li>',
-        //require: '^tmuiGrid',
+        templateUrl: 'app/grid/gridItem.template.html',
         link: link
-      // compile: function(tElem, tAttrs){
-      //
-      //   return {
-      //     pre: function(scope, iElem, iAttrs){
-      //       console.log('second' + ': pre link');
-      //     },
-      //     post: function(scope, iElem, iAttrs){
-      //       console.log('second' + ': post link');
-      //     }
-      //   }
-      // }
       };
       return directive;
 
@@ -47,17 +26,22 @@
           'data-y': scope.item.y
       });
 
+      angular
+        .element($window)
+        .on('resize', onWindowResize);
 
-      //console.log(angular.element("#grid").gridList())
-      // angular.element("#grid").gridList({
-      //   lanes: 9,
-      //   widthHeightRatio: 264 / 294,
-      //   heightToFontSizeRatio: 0.25,
-      //   onChange: function(changedItems) {
-      //     console.log(changedItems);
-      //   }
-      // });
+      el.parent().gridList({
+        lanes: 9,
+        widthHeightRatio: 264 / 294,
+        heightToFontSizeRatio: 0.25,
+        onChange: function(changedItems) {
+          console.log(changedItems);
+        }
+      });
 
+      function onWindowResize() {
+        el.parent().gridList('reflow');
+      }
     };
   }
 })();
